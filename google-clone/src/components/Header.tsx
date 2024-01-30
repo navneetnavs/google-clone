0097992aa1f31839364aff303e268512ba9f198f
navneetnavs <navneetkumar2004@icloud.com>
2024-01-30 16:28:46 +0530

    updated

--- google-clone/src/components/Header.tsx
+++ google-clone/src/components/Header.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 import { TbGridDots } from 'react-icons/tb';
 
 const Header: React.FC = () => {
-    const url: string = "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80";
-    
+    const url: string = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D";
     return (
         <div className="flex justify-end tex-sm pr-4 pt-3 space-x-4 items-center">
             <Link href='https://mail.google.com' className="hover:underline text-sm">Gmail</Link>
